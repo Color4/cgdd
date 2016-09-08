64cd99d546bbd0c292bf6d284a4b1b861c6583d9
Stephen Bridgett <sbridgett@gmail.com>
2016-09-08 23:04:41 +0100

    Updated the text for the FAQ download links.

--- convert_db_to_csv_gzip.sh
+++ convert_db_to_csv_gzip.sh
@@ -14,5 +14,8 @@ OUT="all_dependencies.csv.gz"
 # For Driver's "H.alteration_considered", need to add:  "INNER JOIN gendep_gene H ON (D.driver = H.gene_name)"
 SQL="SELECT D.driver, D.target, D.wilcox_p, D.effect_size, D.zdiff, D.interaction, S.short_name AS study, D.histotype AS tissue, G.entrez_id, G.ensembl_id, G.ensembl_protein_id, G.inhibitors FROM gendep_dependency D INNER JOIN gendep_gene G ON (D.target = G.gene_name) INNER JOIN gendep_study S ON (S.pmid = D.pmid) ORDER BY D.driver, D.wilcox_p ASC;"
 
+# Instead of gzip or zip, could use 7zip which compresses more and can uncompress on Windows easily:
+# Download p7zip for Linux from: https://sourceforge.net/projects/p7zip/files/p7zip/16.02/p7zip_16.02_x86_linux_bin.tar.bz2/download
+# Or on MacOS with homebrew installed:   brew install p7zip
 # The double quotes are needed around the $SQL:
 sqlite3 -batch -header -csv $IN "$SQL" | gzip > $OUT
